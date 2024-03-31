ce4323df3528fcb2d09fc6a308c9899a84cf2171
Michal Rakowski <m.rakowski91@gmail.com>
2024-03-31 18:03:26 +0200

    1.0.5 release

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-// DOM elements (both of these are possible due to google's AB testing changing their UI)
-const buttonContainer = document.querySelector('.IUOThf');
-const tabsContainer = document.querySelector('.crJ18e');
+// DOM elements (all of these can possibly exist due to google's AB testing changing their UI)
+const buttonContainer = document.querySelector('.IUOThf'); // round buttons right below the search input
+const tabsContainer = document.querySelector('.crJ18e'); // tabs right below the search input
 
-const clickableMapThumbnail = document.querySelector('.lu-fs');
+// Using selector array as sometimes multiple different selectors are used for the same element depending on the variant rendered by Google
+const smallMapThumbnailElement = ['.lu-fs', '.V1GY4c']; // small thumbnail with a map, usually on the right side
 const addressMapContainer = document.querySelector('#pimg_1');
 const placesMapContainer = document.querySelector('.S7dMR')
 const countryMapContainer = document.querySelector('.CYJS5e.W0urI.SodP3b.GHMsie.ZHugbd.UivI7b');
 
-
-// build url search query
+// build simple URL search query from search params
 const searchQuery = new URLSearchParams(window.location.search).get('q');
 const parts = new URL(window.location).hostname.split('.');
 const topLevelDomainCode = parts[parts.length - 1];
 const mapsLink = `http://maps.google.${topLevelDomainCode}/maps?q=${searchQuery}`;
 
-// if buttons exist, add the maps button and make the small map thumbnail on the right clickable
+// ---------------------------
+// if buttons exist, add the maps button
 if (buttonContainer) {
     const mapsButton = document.createElement('a');
     mapsButton.classList.add('nPDzT', 'T3FoJb');
@@ -32,14 +33,10 @@ if (buttonContainer) {
     mapsButton.appendChild(mapDiv);
     
     mapsButton && (mapsButton.href = mapsLink);
-    setTimeout(() => {
-        clickableMapThumbnail && (clickableMapThumbnail.parentNode.href = mapsLink);
-    }, 0)
-
     buttonContainer.prepend(mapsButton);
 }
 
-// if tabs exist, add the maps tab and make the small map thumbnail on the right clickable
+// if tabs exist, add the maps tab
 if (tabsContainer) {
     const tabsButton = document.createElement('a');
     tabsButton.classList.add('LatpMc', 'nPDzT', 'T3FoJb');
@@ -49,18 +46,38 @@ if (tabsContainer) {
     mapSpan.textContent = 'Maps';
 
     tabsButton.appendChild(mapSpan);
-    
     tabsButton && (tabsButton.href = mapsLink);
-    setTimeout(() => {
-        clickableMapThumbnail && (clickableMapThumbnail.parentNode.href = mapsLink);
-    }, 0)
 
-    // tabsContainer.prepend(tabsButton);
     if (tabsContainer.children.length > 0) {
         tabsContainer.insertBefore(tabsButton, tabsContainer.firstElementChild.nextSibling);
     } else {
         tabsContainer.appendChild(tabsButton);
     }
+}
+
+// if map thumbnail exists
+if (smallMapThumbnailElement.length) {
+    setTimeout(() => {
+        smallMapThumbnailElement.forEach((elementSelector) => {
+            const targettedElement = document.querySelector(elementSelector);
+            
+            // check if element exists on the page
+            if (targettedElement) {
+                if (targettedElement.parentNode.tagName.toLowerCase() === 'a') {
+                    // if its already an a tag, just update its href attribute with the generated maps link
+                    targettedElement.parentNode.href = mapsLink;
+                } else {
+                    // otherwise create a new a tag with href attribute set to generated maps link, then wrap it around the element
+                    const wrapperLink = document.createElement('a');
+                    wrapperLink.href = mapsLink;
+                    targettedElement.parentNode.insertBefore(wrapperLink, targettedElement);
+                    targettedElement.parentNode.removeChild(targettedElement);
+                    wrapperLink.appendChild(targettedElement);
+                }
+            }
+        });
+        
+    }, 0)
 }
 
 // if address map is shown (the one right below search bar), make it clickable
@@ -75,27 +92,50 @@ if (addressMapContainer) {
 // if places map is shown (the one right below search bar), make it clickable
 if (placesMapContainer) {
     const mapWrapperLinkEl = document.createElement('a');
+    mapWrapperLinkEl.text = 'OPEN IN MAPS';
+    mapWrapperLinkEl.style.cssText = `
+        font-size: 32px;
+        position: absolute;
+        bottom: 0;
+        background: rgba(0, 0, 0, 0.85);
+        text-align: center;
+        color: #fff;
+        cursor: pointer;
+        left: 0;
+        right: 0;
+        height: 60px;
+        line-height: 60px;
+        text-decoration: none; /* Removes underline from link */
+        border: 1px solid rgba(255, 255, 255, 0.2); /* Adds a subtle border */
+        transition: background-color 0.3s ease, color 0.3s ease; /* Smooth transition for hover effects */
+        `;
+
+    placesMapContainer.style.position = 'relative';
     mapWrapperLinkEl && (mapWrapperLinkEl.href = mapsLink);
-
-    placesMapContainer.parentElement.insertBefore(mapWrapperLinkEl, placesMapContainer);
-    mapWrapperLinkEl.appendChild(placesMapContainer);
+    placesMapContainer.append(mapWrapperLinkEl);
 }
-
-// if green tinted country map is shown (the one right below search bar), instead of making it size-extend on click, open google maps instead
+    
+// if "green tinted country map" is shown (the one that appears below search bar), add a new button within that map that allows user to open it 
+// in google maps instead while persisting the normal behavior of extending the map container if clicked within the UI map element
 if (countryMapContainer) {
     const mapWrapperLinkEl = document.createElement('a');
     mapWrapperLinkEl.text = 'OPEN IN MAPS';
-    mapWrapperLinkEl.style.fontSize = '32px';
-    mapWrapperLinkEl.style.position = 'absolute';
-    mapWrapperLinkEl.style.bottom = '0';
-    mapWrapperLinkEl.style.background = 'rgba(0, 0, 0, 0.85)';
-    mapWrapperLinkEl.style.textAlign = 'center';
-    mapWrapperLinkEl.style.color = '#fff';
-    mapWrapperLinkEl.style.cursor = 'pointer';
-    mapWrapperLinkEl.style.left = '0';
-    mapWrapperLinkEl.style.right = '0';
-    mapWrapperLinkEl.style.height = '100px';
-    mapWrapperLinkEl.style.lineHeight = '100px';
+    mapWrapperLinkEl.style.cssText = `
+        font-size: 32px;
+        position: absolute;
+        bottom: 0;
+        background: rgba(0, 0, 0, 0.85);
+        text-align: center;
+        color: #fff;
+        cursor: pointer;
+        left: 0;
+        right: 0;
+        height: 100px;
+        line-height: 100px;
+        text-decoration: none; /* Removes underline from link */
+        border: 1px solid rgba(255, 255, 255, 0.2); /* Adds a subtle border */
+        transition: background-color 0.3s ease, color 0.3s ease; /* Smooth transition for hover effects */
+        `;
 
     mapWrapperLinkEl && (mapWrapperLinkEl.href = mapsLink);
     countryMapContainer.append(mapWrapperLinkEl);
